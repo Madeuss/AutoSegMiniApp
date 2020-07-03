3f4c2a356e8c22310c63891e782d17f045dc1a9f
Madeuss <mateus1two3@gmail.com>
2020-07-03 17:34:30 -0300

    routing the login page

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom"
 // Pages import
 import Home from "./pages/Home"
 import SignUp from "./pages/SignUp"
+import Login from "./pages/Login"
 
 export default function Routes() {
   return (
@@ -11,6 +12,7 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signup" exact component={SignUp} />
+        <Route path="/login" exact component={Login} />
       </Switch>
     </BrowserRouter>
   )
