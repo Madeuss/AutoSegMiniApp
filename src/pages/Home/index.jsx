13796ded88b45211c20c72f592dcdeee9d064c36
Madeuss <mateus1two3@gmail.com>
2020-07-03 15:00:50 -0300

    add a div to fix the   background-image opacity

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,20 +9,23 @@ import marca from "../../assets/marca_mini_app.png"
 
 export default function Home() {
   return (
-    <div className="home-container">
-      <section className="home-images-section">
-        <img src={home_img} alt="Devices example" />
-        <img src={marca} alt="Devices example" />
-      </section>
+    <>
+      <div className="background-opacity" />
+      <div className="home-container">
+        <section className="home-images-section">
+          <img src={home_img} alt="Devices example" />
+          <img src={marca} alt="Devices example" />
+        </section>
 
-      <section className="home-buttons-section">
-        <Button backgroundColor="#EF7734" fontColor="#F9F4C2">
-          Criar Conta
-        </Button>
-        <Button backgroundColor="#D3CE3D" fontColor="#574437">
-          Entrar
-        </Button>
-      </section>
-    </div>
+        <section className="home-buttons-section">
+          <Button backgroundColor="#EF7734" fontColor="#F9F4C2">
+            Criar Conta
+          </Button>
+          <Button backgroundColor="#D3CE3D" fontColor="#574437">
+            Entrar
+          </Button>
+        </section>
+      </div>
+    </>
   )
 }
