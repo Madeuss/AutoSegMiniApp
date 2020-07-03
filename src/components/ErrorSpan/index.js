edaf7904d6154474295a1c67b2f9241695f53bef
Madeuss <mateus1two3@gmail.com>
2020-07-03 16:59:18 -0300

    create the form error span component

---
+++ src/components/ErrorSpan/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const Span = styled.span`
+  color: #ef7734;
+  font-size: 1em;
+  opacity: 0.8;
+  @media (max-width: 500px) {
+    font-size: 2em;
+  }
+`
