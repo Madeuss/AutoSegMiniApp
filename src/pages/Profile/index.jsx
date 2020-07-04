28448ef89153ae5335f2f3225bd7ee39e786e506
Madeuss <mateus1two3@gmail.com>
2020-07-04 13:05:20 -0300

    Create the profile page header

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Header from "../../components/Header";
+import "./styles.css";
+
+function Profile() {
+  return <Header id="profile-header">
+  </Header>;
+}
+
+export default Profile;
