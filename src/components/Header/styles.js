5d892f4d5e692b6357b3fe46b139e3831dedc0f9
Madeuss <mateus1two3@gmail.com>
2020-07-04 13:06:59 -0300

    Create the header component changing logo color

---
+++ src/components/Header/styles.js
@@ -0,0 +1,25 @@
+//Span that is showed on error in form validation
+import styled from "styled-components";
+
+export const Nav = styled.header`
+
+  background-color: #574437;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 200px 0 200px;
+
+  section {
+    display: flex;
+    align-items: center;
+  }
+
+  p {
+    margin-left: 15px;
+    text-transform: Capitalize;
+    color: #F9F4C2;
+    font-family: SF-UL;
+    font-size: 20px;
+  }
+`;
