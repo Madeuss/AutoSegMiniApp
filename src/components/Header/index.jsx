13f8bdae136e52bda73c50d84eff2e7b09ab357d
Madeuss <mateus1two3@gmail.com>
2020-07-04 13:17:41 -0300

    Fix indentation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,13 +9,15 @@ import avatar from "../../assets/avatar.png";
 function Header() {
   const userName = localStorage.getItem("userName");
 
-  return <Nav>
-    <img id="profile-marca-img" src={marca_header} alt="marca" />
-    <section className="profile-info-section">
-      <img src={avatar} alt="Default profile avatar" />
-      <p>{userName}</p>
-    </section>
-  </Nav>;
+  return (
+    <Nav>
+      <img id="profile-marca-img" src={marca_header} alt="marca" />
+      <section className="profile-info-section">
+        <img src={avatar} alt="Default profile avatar" />
+        <p>{userName}</p>
+      </section>
+    </Nav>
+  );
 }
 
 export default Header;
