15980714f8a584435d7143ba019369b9c1b122af
Madeuss <mateus1two3@gmail.com>
2020-07-05 12:16:40 -0300

    create the route to the NewList page

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from "react"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 // Pages import
-import Home from "./pages/Home";
-import SignUp from "./pages/SignUp";
-import Login from "./pages/Login";
-import Profile from "./pages/Profile";
+import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
+import Login from "./pages/Login"
+import Profile from "./pages/Profile"
+import NewList from "./pages/NewList"
 
 export default function Routes() {
   return (
@@ -15,7 +16,8 @@ export default function Routes() {
         <Route path="/signup" exact component={SignUp} />
         <Route path="/login" exact component={Login} />
         <Route path="/profile" exact component={Profile} />
+        <Route path="/list/new" exact component={NewList} />
       </Switch>
     </BrowserRouter>
-  );
+  )
 }
