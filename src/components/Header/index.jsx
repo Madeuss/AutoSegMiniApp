3a6fffb2b9dc7c261662e517ac034dd717598c4d
Madeuss <mateus1two3@gmail.com>
2020-07-05 13:37:04 -0300

    Linking logo to the profile page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,26 @@
-import React from "react";
+import React from "react"
+import { Link } from "react-router-dom"
 
-import { Nav } from "./styles";
+import { Nav } from "./styles"
 
 //images
-import marca_header from "../../assets/marca_app_header.png";
-import avatar from "../../assets/avatar.png";
+import marca_header from "../../assets/marca_app_header.png"
+import avatar from "../../assets/avatar.png"
 
 function Header() {
-  const userName = localStorage.getItem("userName");
+  const userName = localStorage.getItem("userName")
 
   return (
     <Nav>
-      <img id="profile-marca-img" src={marca_header} alt="marca" />
+      <Link to="/profile">
+        <img id="profile-marca-img" src={marca_header} alt="marca" />
+      </Link>
       <section className="profile-info-section">
         <img src={avatar} alt="Default profile avatar" />
         <p>{userName}</p>
       </section>
     </Nav>
-  );
+  )
 }
 
-export default Header;
+export default Header
