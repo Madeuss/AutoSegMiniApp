19df65bfe6a0dee76f551f506c1b3e0433227846
Madeuss <mateus1two3@gmail.com>
2020-07-07 17:07:26 -0300

    Saving the data inserted on redux persist

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import React from "react"
 import Routes from "./routes"
 
 import { Provider } from "react-redux"
-import store from "./store"
+import { PersistGate } from "redux-persist/integration/react"
+import { store, persistor } from "./store"
 
 import "./global.css"
 
 function App() {
   return (
     <Provider store={store}>
-      <Routes />
+      <PersistGate loading={null} persistor={persistor}>
+        <Routes />
+      </PersistGate>
     </Provider>
   )
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,14 @@
 import { createStore } from "redux"
 import { combineReducers } from "redux"
 import { todoReducer } from "./todoList"
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
 
 const rootReducer = combineReducers({ todoReducer })
-const store = createStore(rootReducer)
+const persistConfig = { key: "root", storage }
 
-export default store
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const store = createStore(persistedReducer)
+const persistor = persistStore(store)
+
+export { store, persistor }
